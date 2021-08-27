8592175e78aad18a3c96025d248cb5b4e7754ff9
godusoppp <42061121+godusoppp@users.noreply.github.com>
2021-08-27 11:53:27 +0800

    Update mian.py

--- mian.py
+++ mian.py
@@ -2,18 +2,24 @@
 #安装python3.5以上
 #pip install itchat-uos
 # coding:utf-8
-
 from threading import Thread
-
+import datetime
 import itchat
 from itchat.content import TEXT
+import time
+
 
 
 @itchat.msg_register(TEXT, isGroupChat=True)
 def text_reply(msg):
     if msg.user.NickName == "广州乱斗堂Guangzhou Smash":
-        print(msg.actualNickName)
+        print("\033[0;32;10m%s\033[0m" % msg.actualNickName)
         print(msg.text)
+        if msg.actualNickName in message_user:
+            message_dict[msg.actualNickName] += 1
+        else:
+            message_user.append(msg.actualNickName)
+            message_dict[msg.actualNickName] = 1
 
 
 itchat.auto_login(hotReload=True, statusStorageDir='newInstance.pkl')
@@ -29,3 +35,4 @@ def send_msg():
 thread = Thread(target=send_msg)
 thread.start()
 itchat.run()
+
